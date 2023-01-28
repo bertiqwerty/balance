4fefda2d538d32860b9b1aa46e066ead97ede260
Behrang Shafei <behrang.shafei@basf.com>
2023-01-29 00:29:49 +0100

    multiple plots

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,6 @@
-use egui::plot::{Line, PlotPoints};
-use egui::Context;
+use egui::plot::{Legend, Line, PlotPoints};
+use egui::{Context, Ui};
+use std::fmt::Display;
 use std::mem;
 use std::sync::mpsc::Sender;
 // use web_sys::{Request, RequestInit, RequestMode, Response};
@@ -10,24 +11,47 @@ use crate::core_types::{to_bres, BalResult};
 use crate::io::read_csv_from_str;
 
 #[derive(Debug)]
-enum Download {
+enum Download<'a> {
     None,
-    InProgress,
-    Done(ehttp::Result<ehttp::Response>),
+    InProgress(&'a str),
+    Done((&'a str, ehttp::Result<ehttp::Response>)),
 }
 
 #[derive(PartialEq, Clone)]
-enum VolaAmount {
+enum Vola {
     No,
+    Vo,
     Lo,
     Mi,
     Hi,
 }
+impl Vola {
+    fn to_float(&self) -> f64 {
+        match self {
+            Vola::No => 0.0,
+            Vola::Vo => 0.01,
+            Vola::Lo => 0.05,
+            Vola::Mi => 0.1,
+            Vola::Hi => 0.2,
+        }
+    }
+}
+impl Display for Vola {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            Vola::No => f.write_str("no"),
+            Vola::Vo => f.write_str("vlow"),
+            Vola::Lo => f.write_str("low"),
+            Vola::Mi => f.write_str("mid"),
+            Vola::Hi => f.write_str("high"),
+        }
+    }
+}
 
-fn trigger_dl(url: &str, rx: Sender<Download>, ctx: Context) {
+fn trigger_dl(url: &str, rx: Sender<ehttp::Result<ehttp::Response>>, ctx: Context) {
     let req = ehttp::Request::get(url);
     ehttp::fetch(req, move |response| {
-        match rx.send(Download::Done(response)) {
+        match rx.send(response) {
             Ok(_) => {}
             Err(e) => println!("{:#?}", e),
         };
@@ -35,26 +59,21 @@ fn trigger_dl(url: &str, rx: Sender<Download>, ctx: Context) {
     });
 }
 struct SimInput {
-    vola: VolaAmount,
+    vola: Vola,
     expected_yearly_return: String,
     n_months: String,
 }
 impl SimInput {
     fn new() -> Self {
         SimInput {
-            vola: VolaAmount::Mi,
+            vola: Vola::Mi,
             expected_yearly_return: "".to_string(),
             n_months: "".to_string(),
         }
     }
     fn parse(&self) -> BalResult<(f64, f64, usize)> {
         Ok((
-            match self.vola {
-                VolaAmount::No => 0.0,
-                VolaAmount::Lo => 0.05,
-                VolaAmount::Mi => 0.1,
-                VolaAmount::Hi => 0.2,
-            },
+            self.vola.to_float(),
             self.expected_yearly_return.parse().map_err(to_bres)?,
             self.n_months.parse().map_err(to_bres)?,
         ))
@@ -62,15 +81,20 @@ impl SimInput {
 }
 #[derive(Default, Debug, Clone)]
 struct Chart {
+    name: String,
     dates: Vec<usize>,
     values: Vec<f64>,
 }
 impl Chart {
-    fn new(dates: Vec<usize>, values: Vec<f64>) -> Self {
-        Chart { dates, values }
+    fn new(name: String, dates: Vec<usize>, values: Vec<f64>) -> Self {
+        Chart {
+            name,
+            dates,
+            values,
+        }
     }
-    fn from_tuple((dates, values): (Vec<usize>, Vec<f64>)) -> Self {
-        Self::new(dates, values)
+    fn from_tuple(name: String, (dates, values): (Vec<usize>, Vec<f64>)) -> Self {
+        Self::new(name, dates, values)
     }
     fn to_line(&self) -> Line {
         Line::new(
@@ -80,34 +104,56 @@ impl Chart {
                 .map(|(_, (i, v))| [i as f64, *v])
                 .collect::<PlotPoints>(),
         )
+        .name(self.name.clone())
+    }
+}
+
+#[derive(Default, Clone, Debug)]
+struct Charts {
+    tmp: Chart,
+    added: Vec<Chart>,
+}
+impl Charts {
+    fn adapt_name(&self, name: String) -> String {
+        let exists = self.added.iter().any(|ci| ci.name == name);
+        if exists {
+            format!("{}_{}", name, self.added.len())
+        } else {
+            name
+        }
+    }
+    fn persist_tmp(&mut self) {
+        let mut c = mem::take(&mut self.tmp);
+        let c = Chart::new(self.adapt_name(mem::take(&mut c.name)), c.dates, c.values);
+        self.added.push(c);
     }
 }
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
-pub struct BalanceApp {
-    rx: mpsc::Sender<Download>,
-    tx: mpsc::Receiver<Download>,
-    download: Download,
+pub struct BalanceApp<'a> {
+    rx: mpsc::Sender<ehttp::Result<ehttp::Response>>,
+    tx: mpsc::Receiver<ehttp::Result<ehttp::Response>>,
+    download: Download<'a>,
     status_msg: Option<String>,
-    sim_input: SimInput,
-    tmp_chart: Chart,
+    sim_in: SimInput,
+    charts: Charts,
 }
 
-impl Default for BalanceApp {
+impl<'a> Default for BalanceApp<'a> {
     fn default() -> Self {
         let (rx, tx) = mpsc::channel();
         Self {
             rx,
             tx,
             download: Download::None,
-            tmp_chart: Chart::new(vec![], vec![]),
             status_msg: None,
-            sim_input: SimInput::new(),
+            sim_in: SimInput::new(),
+            charts: Charts::default(),
         }
     }
 }
 
-impl BalanceApp {
+impl<'a> BalanceApp<'a> {
     /// Called once before the first frame.
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         // This is also where you can customize the look and feel of egui using
@@ -115,9 +161,20 @@ impl BalanceApp {
 
         Default::default()
     }
+    pub fn plot(&self, ui: &mut Ui) {
+        //The central panel the region left after adding TopPanel's and SidePanel's
+        egui::plot::Plot::new("month vs price")
+            .legend(Legend::default())
+            .show(ui, |plot_ui| {
+                for c in &self.charts.added {
+                    plot_ui.line(c.to_line())
+                }
+                plot_ui.line(self.charts.tmp.to_line());
+            });
+    }
 }
 
-impl eframe::App for BalanceApp {
+impl<'a> eframe::App for BalanceApp<'a> {
     /// Called by the frame work to save state before shutdown.
 
     /// Called each time the UI needs repainting, which may be many times per second.
@@ -139,28 +196,35 @@ impl eframe::App for BalanceApp {
             ui.heading("Simulate");
             ui.horizontal(|ui| {
                 ui.label("expected yearly return [%]");
-                ui.text_edit_singleline(&mut self.sim_input.expected_yearly_return);
+                ui.text_edit_singleline(&mut self.sim_in.expected_yearly_return);
             });
             ui.horizontal(|ui| {
                 ui.label("vola");
-                ui.radio_value(&mut self.sim_input.vola, VolaAmount::No, "no".to_string());
-                ui.radio_value(&mut self.sim_input.vola, VolaAmount::Lo, "low".to_string());
-                ui.radio_value(&mut self.sim_input.vola, VolaAmount::Mi, "mid".to_string());
-                ui.radio_value(&mut self.sim_input.vola, VolaAmount::Hi, "high".to_string());
+                ui.radio_value(&mut self.sim_in.vola, Vola::No, format!("{}", Vola::No));
+                ui.radio_value(&mut self.sim_in.vola, Vola::Vo, format!("{}", Vola::Vo));
+                ui.radio_value(&mut self.sim_in.vola, Vola::Lo, format!("{}", Vola::Lo));
+                ui.radio_value(&mut self.sim_in.vola, Vola::Mi, format!("{}", Vola::Mi));
+                ui.radio_value(&mut self.sim_in.vola, Vola::Hi, format!("{}", Vola::Hi));
             });
             ui.horizontal(|ui| {
                 ui.label("#months");
-                ui.text_edit_singleline(&mut self.sim_input.n_months);
+                ui.text_edit_singleline(&mut self.sim_in.n_months);
             });
             ui.horizontal(|ui| {
                 if ui.button("simulate").clicked() {
-                    match self.sim_input.parse() {
+                    match self.sim_in.parse() {
                         Ok(data) => {
                             let (noise, expected_yearly_return, n_months) = data;
                             match random_walk(expected_yearly_return, noise, n_months) {
                                 Ok(values) => {
-                                    self.tmp_chart.values = values;
-                                    self.tmp_chart.dates = (0..(n_months + 1)).collect::<Vec<_>>();
+                                    self.charts.tmp.name = self.charts.adapt_name(format!(
+                                        "{}_{}_{}",
+                                        self.sim_in.expected_yearly_return,
+                                        self.sim_in.n_months,
+                                        self.sim_in.vola
+                                    ));
+                                    self.charts.tmp.values = values;
+                                    self.charts.tmp.dates = (0..(n_months + 1)).collect::<Vec<_>>();
                                     self.status_msg = None;
                                 }
                                 Err(e) => {
@@ -173,19 +237,21 @@ impl eframe::App for BalanceApp {
                         }
                     };
                 }
-                if ui.button("add").clicked() {}
+                if ui.button("add").clicked() {
+                    self.charts.persist_tmp();
+                }
             });
             ui.separator();
             ui.heading("Backtest data");
             if ui.button("MSCI EM").clicked() {
                 let url = "https://www.bertiqwerty.com/data/msciem.csv";
                 trigger_dl(url, self.rx.clone(), ctx.clone());
-                self.download = Download::InProgress;
+                self.download = Download::InProgress("MSCI EM");
             }
             if ui.button("MSCI World").clicked() {
                 let url = "https://www.bertiqwerty.com/data/msciworld.csv";
                 trigger_dl(url, self.rx.clone(), ctx.clone());
-                self.download = Download::InProgress;
+                self.download = Download::InProgress("MSCI World");
             }
 
             ui.with_layout(egui::Layout::bottom_up(egui::Align::LEFT), |ui| {
@@ -205,10 +271,10 @@ impl eframe::App for BalanceApp {
 
         egui::CentralPanel::default().show(ctx, |ui| {
             // The central panel the region left after adding TopPanel's and SidePanel's
-            if let Download::InProgress = self.download {
+            if let Download::InProgress(s) = self.download {
                 match self.tx.try_recv() {
                     Ok(d) => {
-                        self.download = d;
+                        self.download = Download::Done((s, d));
                         self.status_msg = None;
                     }
                     _ => {
@@ -216,12 +282,15 @@ impl eframe::App for BalanceApp {
                     }
                 }
             }
-            if let Download::Done(d) = &self.download {
-                self.tmp_chart = match d {
-                    Ok(resp) => Chart::from_tuple(read_csv_from_str(resp.text().unwrap()).unwrap()),
+            if let Download::Done((name, d)) = &self.download {
+                self.charts.tmp = match d {
+                    Ok(resp) => Chart::from_tuple(
+                        name.to_string(),
+                        read_csv_from_str(resp.text().unwrap()).unwrap(),
+                    ),
                     Err(e) => {
                         self.status_msg = Some(format!("{:?}", e));
-                        mem::take(&mut self.tmp_chart)
+                        mem::take(&mut self.charts.tmp)
                     }
                 };
                 self.download = Download::None;
@@ -233,9 +302,7 @@ impl eframe::App for BalanceApp {
                 ui.label("ready");
             }
 
-            //The central panel the region left after adding TopPanel's and SidePanel's
-            egui::plot::Plot::new("month vs price")
-                .show(ui, |plot_ui| plot_ui.line(self.tmp_chart.to_line()));
+            self.plot(ui);
 
             egui::warn_if_debug_build(ui);
         });
