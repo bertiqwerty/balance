314589635c81cded61d80b591bb48360c51bcda8
Behrang Shafei <behrang.shafei@basf.com>
2023-01-28 22:35:31 +0100

    chart struct

--- src/app.rs
+++ src/app.rs
@@ -60,16 +60,37 @@ impl SimInput {
         ))
     }
 }
+#[derive(Default, Debug, Clone)]
+struct Chart {
+    dates: Vec<usize>,
+    values: Vec<f64>,
+}
+impl Chart {
+    fn new(dates: Vec<usize>, values: Vec<f64>) -> Self {
+        Chart { dates, values }
+    }
+    fn from_tuple((dates, values): (Vec<usize>, Vec<f64>)) -> Self {
+        Self::new(dates, values)
+    }
+    fn to_line(&self) -> Line {
+        Line::new(
+            self.dates
+                .iter()
+                .zip(self.values.iter().enumerate())
+                .map(|(_, (i, v))| [i as f64, *v])
+                .collect::<PlotPoints>(),
+        )
+    }
+}
 
 /// We derive Deserialize/Serialize so we can persist app state on shutdown.
 pub struct BalanceApp {
     rx: mpsc::Sender<Download>,
     tx: mpsc::Receiver<Download>,
     download: Download,
-    values: Vec<f64>,
-    dates: Vec<usize>,
     status_msg: Option<String>,
     sim_input: SimInput,
+    tmp_chart: Chart,
 }
 
 impl Default for BalanceApp {
@@ -79,8 +100,7 @@ impl Default for BalanceApp {
             rx,
             tx,
             download: Download::None,
-            values: vec![],
-            dates: vec![],
+            tmp_chart: Chart::new(vec![], vec![]),
             status_msg: None,
             sim_input: SimInput::new(),
         }
@@ -132,26 +152,29 @@ impl eframe::App for BalanceApp {
                 ui.label("#months");
                 ui.text_edit_singleline(&mut self.sim_input.n_months);
             });
-            if ui.button("simulate").clicked() {
-                match self.sim_input.parse() {
-                    Ok(data) => {
-                        let (noise, expected_yearly_return, n_months) = data;
-                        match random_walk(expected_yearly_return, noise, n_months) {
-                            Ok(values) => {
-                                self.values = values;
-                                self.dates = (0..(n_months + 1)).collect::<Vec<_>>();
-                                self.status_msg = None;
-                            }
-                            Err(e) => {
-                                self.status_msg = Some(format!("{:?}", e));
-                            }
-                        };
-                    }
-                    Err(e) => {
-                        self.status_msg = Some(format!("{:?}", e));
-                    }
-                };
-            }
+            ui.horizontal(|ui| {
+                if ui.button("simulate").clicked() {
+                    match self.sim_input.parse() {
+                        Ok(data) => {
+                            let (noise, expected_yearly_return, n_months) = data;
+                            match random_walk(expected_yearly_return, noise, n_months) {
+                                Ok(values) => {
+                                    self.tmp_chart.values = values;
+                                    self.tmp_chart.dates = (0..(n_months + 1)).collect::<Vec<_>>();
+                                    self.status_msg = None;
+                                }
+                                Err(e) => {
+                                    self.status_msg = Some(format!("{:?}", e));
+                                }
+                            };
+                        }
+                        Err(e) => {
+                            self.status_msg = Some(format!("{:?}", e));
+                        }
+                    };
+                }
+                if ui.button("add").clicked() {}
+            });
             ui.separator();
             ui.heading("Backtest data");
             if ui.button("MSCI EM").clicked() {
@@ -194,11 +217,11 @@ impl eframe::App for BalanceApp {
                 }
             }
             if let Download::Done(d) = &self.download {
-                (self.dates, self.values) = match d {
-                    Ok(resp) => read_csv_from_str(resp.text().unwrap()).unwrap(),
+                self.tmp_chart = match d {
+                    Ok(resp) => Chart::from_tuple(read_csv_from_str(resp.text().unwrap()).unwrap()),
                     Err(e) => {
                         self.status_msg = Some(format!("{:?}", e));
-                        (mem::take(&mut self.dates), mem::take(&mut self.values))
+                        mem::take(&mut self.tmp_chart)
                     }
                 };
                 self.download = Download::None;
@@ -210,15 +233,9 @@ impl eframe::App for BalanceApp {
                 ui.label("ready");
             }
 
-            let line = Line::new(
-                self.dates
-                    .iter()
-                    .zip(self.values.iter().enumerate())
-                    .map(|(_, (i, v))| [i as f64, *v])
-                    .collect::<PlotPoints>(),
-            );
             //The central panel the region left after adding TopPanel's and SidePanel's
-            egui::plot::Plot::new("example_plot").show(ui, |plot_ui| plot_ui.line(line));
+            egui::plot::Plot::new("month vs price")
+                .show(ui, |plot_ui| plot_ui.line(self.tmp_chart.to_line()));
 
             egui::warn_if_debug_build(ui);
         });
