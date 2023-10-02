248954a90102d24866d7109ffc1d440773686fad
Behrang Shafei <50267830+bertiqwerty@users.noreply.github.com>
2023-10-02 23:11:58 +0200

    fixed trunk build

--- src/main.rs
+++ src/main.rs
@@ -33,7 +33,7 @@ fn main() {
             .start(
                 "the_canvas_id", // hardcode it
                 web_options,
-                Box::new(|cc| Box::new(balance::BalanceApp::new(cc))),
+                Box::new(|cc| Box::new(rebalance::BalanceApp::new(cc))),
             )
             .await
             .expect("failed to start eframe");
